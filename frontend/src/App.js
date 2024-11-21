fc6b0114523861d929da0123ace2c0e249c6036e
Iknoor Rawal <iknoorrawal@eduroam-campus-10-36-129-120.campus-dynamic.uwaterloo.ca>
2024-11-21 11:53:42 -0500

    Converted frontend from submodule to regular folder

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataFetcherTest from "./components/testing";
+
+function App() {
+    return (
+      <div className="App">
+        <h1>Frontend-Backend Integration Testing</h1>
+            <DataFetcherTest />
+      </div>
+    );
+}
+
+export default App;
